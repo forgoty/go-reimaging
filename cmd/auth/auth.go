6c18410573a4073cd5e25907ff2f6ca187a8221e
Nikita Alkhovik <forgoty13@gmail.com>
2019-12-22 17:26:27 +0300

    Add user client authentication

--- cmd/auth/auth.go
+++ cmd/auth/auth.go
@@ -1,33 +1,19 @@
 package auth
 
 import (
-	"encoding/json"
-	// "fmt"
+	"bufio"
+	"fmt"
 	vkapi "github.com/SevereCloud/vksdk/5.92/api"
-	"io/ioutil"
-	"net/http"
+	"github.com/justblender/vk-api"
+	"golang.org/x/crypto/ssh/terminal"
+	"os"
+	"strings"
+	"syscall"
 )
 
 const (
 	serviceToken string = "66619e0066619e0066d3e34c266634f6666666166619e003ea8d033c12d1a3d08e6fd55"
-	tokenUrl     string = "https://oauth.vk.com/access_token"
-	clientSecret string = "VeWdmVclDCtn6ihuP1nt"
-	clientID     string = "5597286"
 )
-
-type Token struct {
-	AccessToken      string `json:"access_token"`
-	ExpiresIn        int    `json:"expires_in"`
-	UserID           int    `json:"user_id"`
-	Error            string `json:"error"`
-	ErrorDescription string `json:"error_description"`
-}
-
-func (t *Token) String() string {
-	return t.AccessToken
-}
-
-//const AppId string = "5597286"
 
 func GetClient(auth bool) *vkapi.VK {
 	if auth {
@@ -44,45 +30,33 @@ func GetServiceClient() *vkapi.VK {
 func GetUserClient() *vkapi.VK {
 	userToken := getUserToken()
 
-	client := vkapi.Init(userToken.String())
+	client := vkapi.Init(userToken)
 	return client
 }
 
-func getUserToken() Token {
+func getUserToken() string {
 	user, password := getUserCredentials()
-	client := http.Client{}
-	req, error := http.NewRequest("GET", tokenUrl, nil)
-	if error != nil {
-		return Token{}
+	client, err := vk_api.NewClient(vk_api.DirectAuthentication{
+		Username: user,
+		Password: password,
+		Device:   vk_api.ANDROID,
+	})
+	if err != nil {
+		panic("Couldn't authenticate with provided credentials.")
 	}
-	query := req.URL.Query()
-	query.Add("grant_type", "password")
-	query.Add("client_id", clientID)
-	query.Add("client_secret", clientSecret)
-	query.Add("username", user)
-	query.Add("password", password)
-	query.Add("v", "5.92")
-	req.URL.RawQuery = query.Encode()
-
-	response, error := client.Do(req)
-	if error != nil {
-		return Token{}
-	}
-	defer response.Body.Close()
-
-	body, error := ioutil.ReadAll(response.Body)
-	if error != nil {
-		return Token{}
-	}
-
-	var token Token
-	json.Unmarshal(body, &token)
-	if token.Error != "" {
-		return Token{}
-	}
-	return token
+	return client.AccessToken
 }
 
-func getUserCredentials() (login, password string) {
-	return "", "password"
+func getUserCredentials() (string, string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter Username: ")
+	username, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter Password: ")
+	bytePassword, _ := terminal.ReadPassword(int(syscall.Stdin))
+	fmt.Printf("\n\n")
+	password := string(bytePassword)
+
+	return strings.TrimSpace(username), strings.TrimSpace(password)
 }
