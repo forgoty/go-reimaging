75c99a0b553dd8a388001a687df1900f01800ec0
Nikita Alkhovik <forgoty13@gmail.com>
2020-10-09 00:09:14 +0300

    add album-id choice. Some fixes

--- go.mod
+++ go.mod
@@ -4,6 +4,12 @@ go 1.15
 
 require (
 	github.com/SevereCloud/vksdk/v2 v2.4.0
-	github.com/schollz/progressbar v1.0.0
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/schollz/progressbar/v3 v3.6.0
 	github.com/spf13/cobra v1.0.0
+	github.com/spf13/pflag v1.0.5 // indirect
+	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
--- reimaging/download.go
+++ reimaging/download.go
@@ -10,10 +10,11 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/SevereCloud/vksdk/v2/api"
 	"github.com/SevereCloud/vksdk/v2/object"
-	"github.com/schollz/progressbar"
+	progressbar "github.com/schollz/progressbar/v3"
 	"github.com/spf13/cobra"
 
 	"github.com/forgoty/go-reimaging/reimaging/validators"
@@ -60,21 +61,48 @@ func download(args []string) {
 
 	vk := GetVk()
 
-	albums := GetAlbums(vk, userId)
-	for _, album := range albums {
-		downloadAlbum(vk, album)
+	if AlbumId != 0 {
+		downloadAlbumById(vk, userId, AlbumId)
+	} else {
+		downloadAlbums(vk, userId)
 	}
 }
 
-func downloadAlbum(vk *api.VK, album object.PhotosPhotoAlbumFull) error {
-	pathDir := createAlbumDir(album.Title)
+func downloadAlbumById(vk *api.VK, userId, albumId int) {
+	albums := GetAlbums(vk, userId)
+	for _, album := range albums {
+		if album.ID == albumId {
+			downloadAlbum(vk, album)
+		}
+	}
+}
+
+func downloadAlbums(vk *api.VK, userId int) {
+	var wg sync.WaitGroup
+
+	albums := GetAlbums(vk, userId)
+	for _, album := range albums {
+		wg.Add(1)
+		go downloadAlbumWaitGroup(&wg, vk, album)
+	}
+	wg.Wait()
+}
+
+func downloadAlbumWaitGroup(wg *sync.WaitGroup, vk *api.VK, album object.PhotosPhotoAlbumFull) {
+	defer wg.Done()
+	downloadAlbum(vk, album)
+}
+
+func downloadAlbum(vk *api.VK, album object.PhotosPhotoAlbumFull) {
 	offsets := getOffset(album.Size)
 	photosUrls := []string{}
 	for _, offset := range offsets {
 		photosUrls = append(photosUrls, GetPhotoUrls(vk, album.OwnerID, album.ID, offset)...)
 	}
-	downloadPhotos(photosUrls, pathDir)
-	return nil
+	if len(photosUrls) > 0 {
+		pathDir := createAlbumDir(album.Title)
+		downloadPhotos(photosUrls, pathDir, album.Title)
+	}
 }
 
 func createAlbumDir(title string) string {
@@ -100,45 +128,72 @@ func getOffset(size int) []int {
 	return offsets
 }
 
-func downloadPhotos(photosUrls []string, pathDir string) {
-	done := make(chan bool)
-	parallels := 0
-	for _, url := range photosUrls {
-		parallels++
-		go downloadPhoto(url, pathDir, done)
-	}
-	bar := progressbar.New(parallels)
-	for i := 0; i < parallels; i++ {
-		<-done
-		bar.Add(1)
-	}
+type metaDownloadedPhoto struct {
+	url      string
+	filename string
+	path     string
 }
 
-func downloadPhoto(url, pathDir string, done chan bool) {
-	split := strings.Split(url, "/")
-	fileName := strings.ReplaceAll(split[len(split)-1], "-", "")
-	path := pathDir + "/" + fileName
+func downloadPhotos(photosUrls []string, pathDir, albumTitle string) {
+	photosCount := len(photosUrls)
+	done := make(chan bool, photosCount)
+	errch := make(chan error, photosCount)
+	for _, url := range photosUrls {
+		metaImage := getPhotoMetaData(url, pathDir)
+		go downloadPhoto(metaImage, done, errch)
+	}
+	bar := progressbar.Default(int64(photosCount), albumTitle)
+	for i := 0; i < photosCount; i++ {
+		if ok := <-done; !ok {
+			if err := <-errch; err != nil {
+				fmt.Println(err)
+			}
+		}
+		bar.Add(1)
+	}
+	close(done)
+	close(errch)
+}
 
-	if _, err := os.Stat(path); err == nil {
+func getPhotoMetaData(url, pathDir string) metaDownloadedPhoto {
+	split := strings.Split(url, "/")
+	filename := strings.ReplaceAll(split[len(split)-1], "-", "")
+	path := pathDir + "/" + filename
+	return metaDownloadedPhoto{url: url, filename: filename, path: path}
+}
+
+func downloadPhoto(metaImage metaDownloadedPhoto, done chan bool, errch chan error) {
+	_, err := os.Stat(metaImage.path)
+	if err == nil {
+		// file exists
+		done <- true
+		errch <- nil
 		return
 	}
 
-	response, err := http.Get(url)
+	response, err := http.Get(metaImage.url)
 	if err != nil {
-		fmt.Printf("Error while downloading", url)
+		errch <- err
+		done <- false
+		return
 	}
 	defer response.Body.Close()
 
-	output, err := os.Create(path)
+	output, err := os.Create(metaImage.path)
 	if err != nil {
-		fmt.Printf("Error while creating", fileName)
+		errch <- err
+		done <- false
+		return
 	}
 	defer output.Close()
 
 	_, err = io.Copy(output, response.Body)
 	if err != nil {
-		fmt.Printf("Error while saving", url)
+		errch <- err
+		done <- false
+		return
 	}
 	done <- true
+	errch <- nil
 	return
 }
